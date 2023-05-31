dd274b5d276c033fc9768a790e342cb2f2079c9d
MaysLastPlay10 <109207233+MaysLastPlay10@users.noreply.github.com>
2023-05-31 16:46:04 +0300

    update source/FreeplayState.hx

--- source/FreeplayState.hx
+++ source/FreeplayState.hx
@@ -54,8 +54,8 @@ class FreeplayState extends MusicBeatState
 
 	override function create()
 	{
-		//Paths.clearStoredMemory();
-		//Paths.clearUnusedMemory();
+		Paths.clearStoredMemory();
+		Paths.clearUnusedMemory();
 		
 		persistentUpdate = true;
 		PlayState.isStoryMode = false;
