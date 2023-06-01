816da14adb28ea1f7e9d7d632791689879c2d77d
MasterX <95196453+Default-name123@users.noreply.github.com>
2023-06-01 00:21:42 -0500

    update source/PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1609,7 +1609,7 @@ class PlayState extends MusicBeatState
 			return true;
 		}
 
-		var foldersToCheck:Array<String> = [Paths.mods('shaders/')];
+		/*var foldersToCheck:Array<String> = [Paths.mods('shaders/')];
 		if(Paths.currentModDirectory != null && Paths.currentModDirectory.length > 0)
 			foldersToCheck.insert(0, Paths.mods(Paths.currentModDirectory + '/shaders/'));
 
@@ -1646,7 +1646,7 @@ class PlayState extends MusicBeatState
 			}
 		}
 		FlxG.log.warn('Missing shader $name .frag AND .vert files!');
-		return false;
+		return false;*/
 	}
 	#end
 
