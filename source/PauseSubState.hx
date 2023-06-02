5197908fbc86457ad9f18fe81326471da38d190e
MaysLastPlay10 <109207233+MaysLastPlay10@users.noreply.github.com>
2023-06-02 18:42:25 +0300

    update source/PauseSubState.hx

--- source/PauseSubState.hx
+++ source/PauseSubState.hx
@@ -39,6 +39,7 @@ class PauseSubState extends MusicBeatSubstate
 
 		#if mobile
     addVirtualPad(UP_DOWN, A);
+    addVirtualPadCamera(false);
     #end
 
 		if(CoolUtil.difficulties.length < 2) menuItemsOG.remove('Change Difficulty'); //No need to change difficulty if there is only one!
