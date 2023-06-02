96a11df3018c7a0bffd85de495c83bd6013a55e6
MaysLastPlay10 <109207233+MaysLastPlay10@users.noreply.github.com>
2023-06-02 09:08:43 +0300

    update source/CoolUtil.hx

--- source/CoolUtil.hx
+++ source/CoolUtil.hx
@@ -7,12 +7,7 @@ import lime.utils.Assets as LimeAssets;
 import lime.utils.AssetLibrary;
 import lime.utils.AssetManifest;
 import flixel.system.FlxSound;
-#if sys
-import sys.io.File;
-import sys.FileSystem;
-#else
 import openfl.utils.Assets;
-#end
 
 using StringTools;
 
@@ -62,11 +57,8 @@ class CoolUtil
 	public static function coolTextFile(path:String):Array<String>
 	{
 		var daList:Array<String> = [];
-		#if MODS_ALLOWED
-		if(FileSystem.exists(path)) daList = File.getContent(path).trim().split('\n');
-		#else
+
 		if(Assets.exists(path)) daList = Assets.getText(path).trim().split('\n');
-		#end
 
 		for (i in 0...daList.length)
 		{
